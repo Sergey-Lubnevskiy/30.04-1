00a25dc615bfdda16c83992af85311ec1ccf273a
fanda <fanda@DESKTOP-0GTP3PB>
2023-04-30 13:38:27 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp15/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace WindowsFormsApp15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.directoryTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(61, 68);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(100, 20);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // directoryTextBox
+            // 
+            this.directoryTextBox.Location = new System.Drawing.Point(61, 94);
+            this.directoryTextBox.Name = "directoryTextBox";
+            this.directoryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.directoryTextBox.TabIndex = 1;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(61, 120);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.resultTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 185);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.directoryTextBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.TextBox directoryTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
+
---
+++ WindowsFormsApp15/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            string searchWord = searchTextBox.Text;
+            string directoryPath = directoryTextBox.Text;
+
+            if (string.IsNullOrEmpty(searchWord) || string.IsNullOrEmpty(directoryPath))
+            {
+                MessageBox.Show("Please enter a search word and directory path.");
+                return;
+            }
+
+            if (!Directory.Exists(directoryPath))
+            {
+                MessageBox.Show("Directory does not exist.");
+                return;
+            }
+
+            resultTextBox.Clear();
+
+            await SearchFilesAsync(directoryPath, searchWord);
+        }
+
+        private async Task SearchFilesAsync(string directoryPath, string searchWord)
+        {
+            await Task.Run(() =>
+            {
+                string[] files = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+
+                foreach (string file in files)
+                {
+                    int count = CountWordOccurrences(file, searchWord);
+
+                    if (count > 0)
+                    {
+                        resultTextBox.Invoke(new Action(() =>
+                        {
+                            resultTextBox.AppendText($"File name: {Path.GetFileName(file)}" + Environment.NewLine);
+                            resultTextBox.AppendText($"Path: {file}" + Environment.NewLine);
+                            resultTextBox.AppendText($"Occurrences: {count}" + Environment.NewLine + Environment.NewLine);
+                        }));
+                    }
+                }
+            });
+        }
+
+        private int CountWordOccurrences(string filePath, string searchWord)
+        {
+            int count = 0;
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    count += (line.ToLower().Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries)).Count(word => word == searchWord.ToLower());
+                }
+            }
+
+            return count;
+        }
+    }
+}
